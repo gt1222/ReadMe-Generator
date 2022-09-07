16b4569154b2d51e17546f89b03545d89ca22052
Mona Lisa <giatran1222@gmail.com>
2022-09-06 22:26:57 -0700

    created writeFile and init function

--- Starter-Code/index.js
+++ Starter-Code/index.js
@@ -60,7 +60,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data) {
+        writeToFile("ReadMe.md", generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
